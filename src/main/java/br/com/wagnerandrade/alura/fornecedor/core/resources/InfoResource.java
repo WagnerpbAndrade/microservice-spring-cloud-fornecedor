2d7117daea54ec106a7928176b5b8a838b214a5f
Wagner Andrade <wagnerpbandrade@gmail.com>
2021-02-17 20:32:06 -0300

    Distributed Tracing e Spring Sleuth

--- src/main/java/br/com/wagnerandrade/alura/fornecedor/core/resources/InfoResource.java
+++ src/main/java/br/com/wagnerandrade/alura/fornecedor/core/resources/InfoResource.java
@@ -3,6 +3,8 @@ package br.com.wagnerandrade.alura.fornecedor.core.resources;
 import br.com.wagnerandrade.alura.fornecedor.core.services.InforService;
 import br.com.wagnerandrade.alura.fornecedor.core.transport.InfoFornecedorDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InfoResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InfoResource.class);
+
     private final InforService inforService;
 
     @GetMapping("/{estado}")
     public ResponseEntity<InfoFornecedorDTO> getInfoPorEstado(@PathVariable String estado) {
+        LOG.info("Recebido pedido de informações de fornecedor de {}" + estado);
         return ResponseEntity.ok().body(this.inforService.getInfoPorEstado(estado));
     }
 }
--- src/main/java/br/com/wagnerandrade/alura/fornecedor/core/resources/PedidoResource.java
+++ src/main/java/br/com/wagnerandrade/alura/fornecedor/core/resources/PedidoResource.java
@@ -4,6 +4,8 @@ import br.com.wagnerandrade.alura.fornecedor.core.entities.Pedido;
 import br.com.wagnerandrade.alura.fornecedor.core.services.PedidoService;
 import br.com.wagnerandrade.alura.fornecedor.core.transport.ItemDoPedidoDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,11 +14,13 @@ import java.util.List;
 @RequestMapping("/api/v1/pedidos")
 @RequiredArgsConstructor
 public class PedidoResource {
+	private static final Logger LOG = LoggerFactory.getLogger(PedidoResource.class);
 
 	private final PedidoService pedidoService;
 	
 	@PostMapping
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+		LOG.info("Pedido recebido");
 		return pedidoService.realizaPedido(produtos);
 	}
 	
